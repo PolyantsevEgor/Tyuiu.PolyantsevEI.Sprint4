0243d70d3b729996d2895aa90c01e91f305036ce
Tyuiu.PolyantsevEI@outlook.com <qreft@mail.ru>
2023-11-04 17:16:22 +0500

    Add project files.

---
+++ Tyuiu.PolyantsevEI.Sprint4.Task0.V26.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.PolyantsevEI.Sprint4.Task0.V26.Lib;
+
+namespace Tyuiu.PolyantsevEI.Sprint4.Task0.V26.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumOddArrEl()
+        {
+            DataService ds = new DataService();
+            int[] array = new int[] { 9, 3, 7, 1, 5, 5, 3, 2, 1, 7 };
+            int res = ds.GetSumOddArrEl(array);
+            int wait = 41;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.PolyantsevEI.Sprint4.Task0.V26/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.PolyantsevEI.Sprint4.Task0.V26.Lib;
+
+namespace Tyuiu.PolyantsevEI.Sprint4.Task0.V26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Полянцев Е. И. | ИСТНб-23-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #26                                                             *");
+            Console.WriteLine("* Выполнил: Полянцев Егор Ильич | ИСТНб-23-1                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов заполненный         *");
+            Console.WriteLine("* статическими значениями в диапазоне от 0 до 9. Подсчитать сумму         *");
+            Console.WriteLine("* нечетных элементов массива.                                             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Массив: ");
+            int[] array = new int[] { 9, 3, 7, 1, 5, 5, 3, 2, 1, 7 };
+            for (int i = 0; i < array.Length; i++) Console.WriteLine(array[i]);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма нечётных элементов = " + ds.GetSumOddArrEl(array));
+
+            Console.ReadKey();
+        }
+    }
+}
